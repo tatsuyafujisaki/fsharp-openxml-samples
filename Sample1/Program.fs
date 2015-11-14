e75a6c4c695d1ce49869a85a40b0a876d20462cb
Tatsuya Fujisaki <tatsuya.fujisaki@example.com>
2015-11-14 12:33:55 +0900

    First commit.

---
+++ Sample1/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.Linq
+open DocumentFormat.OpenXml.Packaging
+open DocumentFormat.OpenXml.Wordprocessing
+
+[<EntryPoint>]
+let main _ =
+    use wd = WordprocessingDocument.Open(@"C:\foo.docx", false)
+    wd.MainDocumentPart
+        .Document
+        .Body
+        .Elements<Paragraph>()
+        .FirstOrDefault()
+        .FirstChild
+        .ElementsAfter()
+        |> Seq.filter (fun element -> element :? Run)
+        |> Seq.iter (fun run -> printfn "%A" run.InnerText)
+
+    Console.ReadKey() |> ignore
+    0
---
+++ Sample2/Program.fs
@@ -0,0 +1,47 @@
+﻿open DocumentFormat.OpenXml
+open DocumentFormat.OpenXml.Packaging
+open DocumentFormat.OpenXml.Wordprocessing
+
+let createTableBorders size =
+    let size' = UInt32Value (uint32 size)
+    let tbs = new TableBorders()
+    tbs.AppendChild(new TopBorder (Val = new EnumValue<BorderValues>(BorderValues.Single), Size = size')) |> ignore
+    tbs.AppendChild(new BottomBorder (Val = new EnumValue<BorderValues>(BorderValues.Single), Size = size')) |> ignore
+    tbs.AppendChild(new LeftBorder (Val = new EnumValue<BorderValues>(BorderValues.Single), Size = size')) |> ignore
+    tbs.AppendChild(new RightBorder (Val = new EnumValue<BorderValues>(BorderValues.Single), Size = size')) |> ignore
+    tbs.AppendChild(new InsideHorizontalBorder (Val = new EnumValue<BorderValues>(BorderValues.Single), Size = size')) |> ignore
+    tbs.AppendChild(new InsideVerticalBorder (Val = new EnumValue<BorderValues>(BorderValues.Single), Size = size')) |> ignore
+    tbs
+
+type E = OpenXmlElement
+
+let createTable (data : string[,]) borderSize =
+    let tps = new TableProperties()
+    let tbs = createTableBorders borderSize
+    tps.AppendChild(tbs) |> ignore
+
+    let table = new Table(tps :> OpenXmlElement)
+    for i = 0 to data.GetUpperBound(0) do
+        let tr = new TableRow()
+        for j = 0 to data.GetUpperBound(1) do
+            tr.Append(new TableCell(new Paragraph(new Run(new Text(data.[i, j]) :> E) :> E) :> E,
+                        new TableCellProperties(new TableCellWidth (Width = StringValue "100", Type = EnumValue TableWidthUnitValues.Pct)
+                        :> E) :> E) :> E)
+        table.Append(tr :> E)
+    table
+
+[<EntryPoint>]
+let main argv = 
+    let filePath = @"C:\x\result.docx"
+    let text = "Test of creating a table."
+    let data = array2D [|[| "a"; "b" |]; [| "c"; "d" |]|]
+
+    use wd = WordprocessingDocument.Create(filePath, WordprocessingDocumentType.Document)
+    wd.AddMainDocumentPart().Document <- new Document(new Body(new Paragraph(new Run(new Text(text) :> E) :> E) :> E) :> E) 
+
+    let table = createTable data 1
+
+    let doc = wd.MainDocumentPart.Document
+    doc.Body.AppendChild(table) |> ignore
+    doc.Save()
+    0
